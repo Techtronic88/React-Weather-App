53c66071ea946f7f521b868e0a68db49eaf4ac53
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-04-26 11:11:30 +0700

    Add files via upload
    Updated: Making the sure the data input from users is true before we fetch the data

--- App.js
+++ App.js
@@ -7,18 +7,47 @@ const API_KEY = "9d3221eea82df792852a7272ce12430d";
 
 class App extends Component {
 
+  state = {
+    temperature: '',
+    city: '',
+    humidity: '',
+    description: '',
+    error: ''
+  }
+
   getWeather = async(e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+
+
+    // API calls return data object we can start going down the object chain from there.
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
+    if( city && country) {
+    this.setState({ // Only run the code when both left and right of && is true. Force user to put in input
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: "" 
+    })
   }
+}
   render() {
     return (
       <div>
       <Titles />
       <Form getWeather={this.getWeather}/>    
-      <Weather /> 
+      <Weather temperature = {this.state.temperature}
+               city = {this.state.city}
+               country = {this.state.country}
+               humidity = {this.state.humidity}
+               description = {this.state.description}
+               error={this.state.error}        
+  
+      /> 
       </div>
 
     );
--- components/Weather.js
+++ components/Weather.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
 class Weather extends Component {
+
+    // NOTE TO SELF: In render block you cannot use comment the traditional way
     render() {
         return (
             <div>
-                Form Components
+
+            {/* Since JSX does not like if/ else statement, we utilized the && method below 
+                left hand side of && represents condition - Right hand side is action shoudld left hand side === true */}
+            {this.props.city && <p> Location: { this.props.city }</p>}
+            {this.props.country && <p> Country: { this.props.country } </p>}
+            {this.props.temperature && <p> Temperature: { this.props.temperature } </p>} 
+            {this.props.humidity && <p> Humidity:  { this.props.humidity }</p>}
+            {this.props.condition && <p> Condition: { this.props.description }</p>}
+
             </div>
         );
     }
