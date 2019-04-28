97a4d540f2ded05d8d8c5618b546efcdf7a88a69
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-04-28 07:01:45 +0700

    Add files via upload
    Set the CSS for block of text display such as  weather, city, country and humidity information. 
    Implemented prop using functional stateless with props

--- components/Weather.js
+++ components/Weather.js
@@ -1,23 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Weather extends Component {
+const Weather = props =>  (
 
     // NOTE TO SELF: In render block you cannot use comment the traditional way
-    render() {
-        return (
-            <div>
+    
+            <div className="weather__info">
 
             {/* Since JSX does not like if/ else statement, we utilized the && method below 
                 left hand side of && represents condition - Right hand side is action shoudld left hand side === true */}
-            {this.props.city && <p> Location: { this.props.city }</p>}
-            {this.props.country && <p> Country: { this.props.country } </p>}
-            {this.props.temperature && <p> Temperature: { this.props.temperature } </p>} 
-            {this.props.humidity && <p> Humidity:  { this.props.humidity }</p>}
-            {this.props.condition && <p> Condition: { this.props.description }</p>}
-            {this.props.error && <p> {this.props.error}</p>}
+            {props.city && <p className="weather__key"> Location: 
+                <span className="weather__value"> { props.city }</span>
+                </p>
+            }
+
+
+            {props.country && <p className="weather__key"> Country: 
+               <span className="weather__value"> { props.country } </span>
+               </p>
+            }
+            {props.temperature && <p className="weather__key"> Temperature: 
+                <span className="weather__value"> { props.temperature } </span>
+                 </p>
+        } 
+            {props.humidity && <p className="weather__key"> Humidity:  
+                <span className="weather__value"> { props.humidity } </span>
+                </p>
+            }
+            {props.condition && <p className="weather__key"> Condition: 
+                <span className="weather__value"> { props.description } </span>
+                </p>
+            }
+            {props.error && <p className="weather__key"> {props.error}</p>}
             </div>
-        );
-    }
-}
+        ); 
 
 export default Weather
